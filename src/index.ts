377019c9361a0b2aa33e9ad00ea044ec5aaeb08f
imranhassan96 <imranhassan96>
2020-07-03 17:12:40 +0500

    additional config

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+interface Color {
+    red: number,
+    blue: number,
+    white: number
+}
+const color: Color = {
+    red: 12,
+    blue: 13,
+    white: 14
+}
+
+console.log(color)
+
+export default color
