202e73f85e45cd36009fe924c3b5198c453e9266
imranhassan96 <imranhassan96>
2020-07-03 20:00:53 +0500

    added not-found-error

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ export * from './errors/database-connection-error'
 export * from './errors/not-authorized-error'
 export * from './errors/request-validation-error'
 export * from './errors/validate-req'
+export * from './errors/not-found-error'
 
 export * from './middlewares/current-user'
 export * from './middlewares/error-handler'
