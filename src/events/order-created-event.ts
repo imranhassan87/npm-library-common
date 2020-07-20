7dede47e9e5432e0abc149413c3efeefbd64732c
imranhassan87 <imranhassan926@gmail.com>
2020-07-20 21:25:33 +0500

    added version number to events

--- src/events/order-created-event.ts
+++ src/events/order-created-event.ts
@@ -5,6 +5,7 @@ export interface OrderCreatedEvent {
     subject: Subjects.OrderCreated
     data: {
         id: string
+        version: number
         userId: string,
         expiresAt: string,
         status: OrderStatus,
